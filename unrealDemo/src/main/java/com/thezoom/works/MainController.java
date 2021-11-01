848e9d45311734a35b49a8d9bec7c9e99403f0f4
thezo <thezo@DESKTOP-M55G3PU>
2021-11-01 09:39:54 +0900

    Unreal Demo Main Resources upload

--- unrealDemo/src/main/java/com/thezoom/works/MainController.java
+++ unrealDemo/src/main/java/com/thezoom/works/MainController.java
@@ -9,4 +9,47 @@ public class MainController {
 	public String main() {
 		return "index";
 	}
+	
+	@RequestMapping(value="/index.html")
+	public String index() {
+		return "index";
+	}
+	
+	
+	@RequestMapping(value="/image-1.html")
+	public String image1() {
+		return "image-1";
+	}
+	
+	@RequestMapping(value="/image-2.html")
+	public String image2() {
+		return "image-2";
+	}
+	
+	@RequestMapping(value="/image-3.html")
+	public String image3() {
+		return "image-3";
+	}
+	
+	// Test Area
+	
+	@RequestMapping(value="/maintest")
+	public String maintest() {
+		return "maintest";
+	}
+	
+	@RequestMapping(value="/test")
+	public String test() {
+		return "test";
+	}
+	
+	@RequestMapping(value="/test2")
+	public String test2() {
+		return "test2";
+	}
+	
+	@RequestMapping(value="/p")
+	public String pixel() {
+		return "PixelDemo";
+	}
 }
